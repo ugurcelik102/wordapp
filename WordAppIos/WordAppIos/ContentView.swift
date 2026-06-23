1b187bfb76847024825c75ae5e82ef6bcfb1a861
ugur <ugur@aydevo.com>
2026-06-23 13:44:53 +0300

    Initial Commit

---
+++ WordAppIos/WordAppIos/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WordAppIos
+//
+//  Created by ugur_celik on 23.06.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WordAppIos/WordAppIos/WordAppIosApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordAppIosApp.swift
+//  WordAppIos
+//
+//  Created by ugur_celik on 23.06.2026.
+//
+
+import SwiftUI
+
+@main
+struct WordAppIosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
